f5858f0f9716396cd0e08f6bbc66bbe6ffb42672
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-06-10 21:40:49 +0000

    Card create, Randomize, button interaction

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+function newCard() {
+  let icons = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomIcon = Math.floor(Math.random() * icons.length);
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector("#top").innerHTML = icons[randomIcon];
+  document.querySelector("#number").innerHTML = numbers[randomNumber];
+  document.querySelector("#bottom").innerHTML = icons[randomIcon];
+
+  let colorSuit =
+    icons[randomIcon] === "♦" || icons[randomIcon] === "♥"
+      ? "text-danger"
+      : "text-dark";
+  document.querySelector("#top").className = colorSuit;
+  document.querySelector("#number").className = colorSuit;
+  document.querySelector("#bottom").className = colorSuit;
+
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", event => {
+    newCard();
+  });
+}
